1cdc4f7a028a09c87838ccfbf3d03fcebe8f19df
AngelicaRoy <ariadna.espiroy@gmail.com>
2022-04-24 18:09:48 -0500

    update missionComander test

--- test/missionComander.test.js
+++ test/missionComander.test.js
@@ -1,11 +1,12 @@
 const MissionComander = require('./../app/missionComander');
+//Agregando prueba 1
 describe("Esto es una suite de pruebas", () => {
     test('Caso de prueba 1', () => {
       const result = 1 + 2 
       expect(result).toBe(3);
     });
 })
-  
+//Agregando prueba 2  
 describe("Unit Tests for Mission Comander Class", () => {
     test('1) Create a mission commander objet', () => {
         const myMissionComander = new MissionComander("Woopa")
